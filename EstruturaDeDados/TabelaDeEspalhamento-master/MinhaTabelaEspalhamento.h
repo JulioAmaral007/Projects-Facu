4288c498d384935010e67fb53ed80ec76aa06f79
Julio Cezar <captaingamergroot@gmail.com>
2023-11-03 11:45:18 -0300

    feat: finish tabela de espalhamento

--- EstruturaDeDados/TabelaDeEspalhamento-master/MinhaTabelaEspalhamento.h
+++ EstruturaDeDados/TabelaDeEspalhamento-master/MinhaTabelaEspalhamento.h
@@ -1,6 +1,7 @@
 #ifndef MINHA_TABELA_ESPALHAMENTO_H
 #define MINHA_TABELA_ESPALHAMENTO_H
 
+
 #include "MinhaListaEncadeada.h"
 // MinhaListaEncadeada
 #include "TabelaEspalhamentoAbstrata.h"
@@ -8,111 +9,127 @@
 #include "excecoes.h"
 // ExcecaoDadoInexistente
 
+
 #include <typeinfo>
 #include <cmath>
 
+
 #include <array>
-// std::array
 
-template <typename T, std::size_t _capacidade>
-class MinhaTabelaEspalhamento final : public TabelaEspalhamentoAbstrata<T, _capacidade>
+
+template<typename T, std::size_t capac>
+class MinhaTabelaEspalhamento final:
+    public TabelaEspalhamentoAbstrata<T, capac>
 {
-public:
-    ~MinhaTabelaEspalhamento()
-    {
-    }
+    protected:
+        std::size_t funcaoEspalhamento(std::string dado) const {
+            std::size_t value = 0;
+            std::size_t n = 31; // Fator de espalhamento
 
-    /**
-     * @brief Obtém a capacidade da tabela.
-     * 
-     * @return Um inteiro maior do que 0.
-     */
-    std::size_t capacidade() const
-    {
-        return this->_tabela.size();
-    };
 
-    /**
-     * @brief Insere um dado na tabela. Se a tabela já contiver o dado, ele não
-     * é inserido novamente.
-     * 
-     * @param dado O dado a ser inserido.
-     */
-    void inserir(T dado)
-    {
-        std::size_t chaveDivisao = funcaoEspalhamento(dado);
-        this->_tabela.at(chaveDivisao).adicionaNoFim(dado);
-    };
+            // Itera pelos caracteres na string e calcula o valor de espalhamento.
+            for (std::size_t i = 0; i < dado.length(); i++) {
+                value += static_cast<std::size_t>(dado[i]) * std::pow(n, dado.length() - (i + 1));
+            }
 
-    /**
-     * @brief Remove um dado da tabela. Se a tabela não contiver o dado, uma
-     * exceção ExcecaoDadoInexistente é lançada.
-     * 
-     * @param dado O dado a ser removido.
-     */
-    void remover(T dado)
-    {
-        try
-        {
-            std::size_t chaveDivisao = funcaoEspalhamento(dado);
-            this->_tabela.at(chaveDivisao).retiraEspecifico(dado);
+
+            // Calcula o índice final usando o módulo com a capacidade da tabela hash.
+            std::size_t tDado = (value % capacidade());
+            return tDado;
         }
-        catch (const char *msg)
+
+
+    public:
+        ~MinhaTabelaEspalhamento()
         {
-            throw ExcecaoDadoInexistente();
         }
-    };
 
-    /**
-     * @brief Verifica se \p dado está contido na tabela.
-     * 
-     * @param dado O dado sendo buscado.
-     * @return true se o dado está contido na tabela; false caso contrário.
-     */
-    bool contem(T dado) const
-    {
-        std::size_t chaveDivisao = funcaoEspalhamento(dado);
-        return this->_tabela.at(chaveDivisao).contem(dado);
-    };
 
-    /**
-     * @brief Obtém a quantidade de dados contidos na árvore.
-     * 
-     * @return Um inteiro maior ou igual a 0.
-     */
-    std::size_t quantidade() const
-    {
-        std::size_t size = 0;
-        for (size_t i = 0; i < this->_tabela.size(); i++)
-            size += this->_tabela.at(i).getTamanho();
-
-        return size;
-    };
-
-protected:
-    std::size_t funcaoEspalhamento(std::string dado) const
-    {
-        std::size_t value = 0;
-        for (std::size_t i = 0; i < dado.length(); i++)
+        /**
+         * @brief Obtém a capacidade da tabela.
+         *
+         * @return Um inteiro maior do que 0.
+         */
+        std::size_t capacidade() const
         {
-            std::size_t n = 31;
-            std::size_t teste = dado[i] * pow(n, dado.length() - (i + 1));
-            value += teste;
-        }
-        std::cout << value << std::endl;
+            return this->tabela.size();
+        };
 
-        std::size_t chaveDivisao = (value % this->_tabela.size());
-        return chaveDivisao;
-    };
 
-    std::size_t funcaoEspalhamento(int dado) const
-    {
+        /**
+         * @brief Insere um dado na tabela. Se a tabela já contiver o dado, ele não
+         * é inserido novamente.
+         *
+         * @param dado O dado a ser inserido.
+         */
+        void inserir(T dado)
+        {
+            std::size_t tDado = funcaoEspalhamento(dado);
+            if(!this->tabela.at(tDado).contem(dado))
+                this->tabela.at(tDado).inserirNoFim(dado);
+        };
 
-        std::size_t value = (std::size_t)dado;
 
-        std::size_t chaveDivisao = (value % this->_tabela.size());
-        return chaveDivisao;
-    };
+        /**
+         * @brief Remove um dado da tabela. Se a tabela não contiver o dado, uma
+         * exceção ExcecaoDadoInexistente é lançada.
+         *
+         * @param dado O dado a ser removido.
+         */
+        void remover(T dado)
+        {
+            std::size_t tDado = funcaoEspalhamento(dado);
+            if(!this->tabela.at(tDado).contem(dado))
+                throw ExcecaoDadoInexistente();
+            this->tabela.at(tDado).remover(dado);
+        };
+
+
+        /**
+         * @brief Verifica se \p dado está contido na tabela.
+         *
+         * @param dado O dado sendo buscado.
+         * @return true se o dado está contido na tabela; false caso contrário.
+         */
+        bool contem(T dado) const
+        {
+            std::size_t tDado = funcaoEspalhamento(dado);
+            if(this->tabela.at(tDado).contem(dado))
+                return true;
+            return false;
+        };
+
+
+        /**
+         * @brief Obtém a quantidade de dados contidos na árvore.
+         *
+         * @return Um inteiro maior ou igual a 0.
+         */
+        std::size_t quantidade() const
+        {
+            std::size_t size = 0;
+            for (size_t i = 0; i < capacidade(); i++)
+                size += this->tabela.at(i).tamanho();
+            return size;
+        };
+
+
+        std::size_t funcaoEspalhamento(int dado) const
+        {
+        // Converte o valor inteiro em um valor sem sinal de tamanho.
+        std::size_t value = static_cast<std::size_t>(dado);
+
+
+        // Calcula o índice usando a operação de módulo com o tamanho da tabela.
+        std::size_t tDado = value % this->tabela.size();
+        return tDado;
+        };
 };
 
+
 #endif
+
+
+
+
+
